606132f2311c05d8dc285ee324fb4d06816a76a4
Quan Truong <qmtruong92@gmail.com>
2018-10-12 11:01:47 -0700

    connected app to mongoose db

--- server/app.js
+++ server/app.js
@@ -2,8 +2,15 @@ const express = require('express');
 // convention is graphqlHTTP
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect('mongodb://quan:test123@ds131373.mlab.com:31373/gql-ninja');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
